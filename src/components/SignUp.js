cce2814b9975402354f713229bdbb697118af67f
cedricantwi <cedric.antwi@outlook.com>
2024-09-16 23:06:29 +0100

    project file cleanup

---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+
+export function SignUp(){
+    return (
+        <div className="app-signup-container">
+            <form className="app-signup-form">
+                <input>
+            </form>
+        </div>
+    )
+}
